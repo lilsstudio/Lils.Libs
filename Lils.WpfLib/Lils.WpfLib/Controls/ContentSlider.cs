676fa1242244f1192dac920640597f92d5cd4559
lymims <lymims@outlook.com>
2019-06-30 14:20:36 +0800

    add ContentSlider

--- Lils.WpfLib/Lils.WpfLib/Controls/ContentSlider.cs
+++ Lils.WpfLib/Lils.WpfLib/Controls/ContentSlider.cs
@@ -1,25 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Markup;
 using System.Windows.Shapes;
+using Lils.WpfLib.Tools;
 
 namespace Lils.WpfLib.Controls
 {
-    public class ContentSlider : Control
+    /// <summary>
+    /// A slider can fill with content
+    /// </summary>
+    [TemplatePart(Name = "PART_ContentGrid", Type = typeof(Grid))]
+    [TemplatePart(Name = "PART_Pointer", Type = typeof(UIElement))]
+    public class ContentSlider : ContentControl
     {
+        #region private fields
+
+        private const string ContentGridTemplateName = "PART_ContentGrid";
+        private const string PointTemplateName = "PART_Pointer";
+
+        private Grid contentGrid;
+        private UIElement pointer;
+
+        #endregion
+
         static ContentSlider()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentSlider), new FrameworkPropertyMetadata(typeof(ContentSlider)));
+        }
+
+        public ContentSlider()
+        {
+            HorizontalContentAlignment = HorizontalAlignment.Stretch;
+            VerticalContentAlignment = VerticalAlignment.Stretch;
+        }
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        [Category("Content")]
+        public object Pointer
+        {
+            get { return (object)GetValue(PointerProperty); }
+            set { SetValue(PointerProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(ContentSlider), new PropertyMetadata(0.0));
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(ContentSlider), new PropertyMetadata(1.0));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(ContentSlider), new PropertyMetadata(0.0));
+
+        public static readonly DependencyProperty PointerProperty =
+            DependencyProperty.Register("Pointer", typeof(object), typeof(ContentSlider), new PropertyMetadata(null));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            contentGrid = GetTemplateChild(ContentGridTemplateName) as Grid;
+            contentGrid.MouseDown += ContentGrid_MouseAction;
+            contentGrid.MouseMove += ContentGrid_MouseAction;
+            contentGrid.MouseUp += ContentGrid_MouseUp;
+
+            pointer = GetTemplateChild(PointTemplateName) as UIElement;
+        }
+
+        private void ContentGrid_MouseAction(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                return;
+            }
+            var point = e.GetPosition(contentGrid);
+            point = point.Coerce(0, contentGrid.ActualHeight);
+            Canvas.SetTop(pointer, point.Y);
+
+            var ratio = 1 - (point.Y / contentGrid.ActualHeight);
+            Value = (Maximum - Minimum) * ratio + Minimum;
+
+            Mouse.Capture(contentGrid);
+        }
+
+        private void ContentGrid_MouseUp(object sender, MouseEventArgs e)
+        {
+            Mouse.Capture(null);
         }
     }
 }
---
+++ Lils.WpfLib/Lils.WpfLib/Tools/MathTools.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lils.WpfLib.Tools
+{
+    static class MathTools
+    {
+        /// <summary>
+        /// coerce the current double value is in range [min, max]
+        /// </summary>
+        /// <param name="value">current value</param>
+        /// <param name="min">min value</param>
+        /// <param name="max">max value</param>
+        /// <returns>coerced value</returns>
+        public static double Coerce(this double value, double min, double max)
+        {
+            if (min > max)
+            {
+                var message = $"{nameof(min)} must lesser than {nameof(max)}";
+                throw new ArgumentException(message);
+            }
+            value = Math.Max(value, min);
+            value = Math.Min(value, max);
+            return value;
+        }
+
+        /// <summary>
+        /// coerce the current point value is in the rect range [(min, min) (max, max)]
+        /// </summary>
+        /// <param name="value">current value</param>
+        /// <param name="min">coordinate x and y of the min point</param>
+        /// <param name="max">coordinate x and y of the max point</param>
+        /// <returns>coerced value</returns>
+        public static Point Coerce(this Point value, double min, double max)
+        {
+            if (min > max)
+            {
+                var message = $"{nameof(min)} must lesser than {nameof(max)}";
+                throw new ArgumentException(message);
+            }
+            return value.Coerce(new Point(min, min), new Point(max, max));
+        }
+
+        /// <summary>
+        /// coerce the current point value is in the rect range [min, max]
+        /// </summary>
+        /// <param name="value">current value</param>
+        /// <param name="min">min value</param>
+        /// <param name="max">max value</param>
+        /// <returns></returns>
+        public static Point Coerce(this Point value, Point min, Point max)
+        {
+            if (min.X > max.X)
+            {
+                var message = $"{nameof(min)}.X must lesser than {nameof(max)}.X";
+                throw new ArgumentException(message);
+            }
+            if (min.Y > max.Y)
+            {
+                var message = $"{nameof(min)}.Y must lesser than {nameof(max)}.Y";
+                throw new ArgumentException(message);
+            }
+
+            value.X = Math.Max(value.X, min.X);
+            value.X = Math.Min(value.X, max.X);
+            value.Y = Math.Max(value.Y, min.Y);
+            value.Y = Math.Min(value.Y, max.Y);
+
+            return value;
+        }
+    }
+}
