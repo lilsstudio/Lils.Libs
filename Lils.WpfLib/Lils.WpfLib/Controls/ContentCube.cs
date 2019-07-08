55cb5649ecd45335ca7b13c5c2c80f9fdd791e54
lymimt <lymimt@outlook.com>
2019-07-08 16:42:00 +0800

    update ContentCube (incomplete)

--- Lils.WpfLib/Lils.WpfLib/Controls/ContentCube.cs
+++ Lils.WpfLib/Lils.WpfLib/Controls/ContentCube.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Media3D;
 
 namespace Lils.WpfLib.Controls
 {
@@ -18,6 +19,8 @@ namespace Lils.WpfLib.Controls
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentCube), new FrameworkPropertyMetadata(typeof(ContentCube)));
         }
+
+        GeometryModel3D layerGeometryModel;
 
         public ContentCube()
         {
@@ -41,6 +44,18 @@ namespace Lils.WpfLib.Controls
             set { SetValue(XZContentProperty, value); }
         }
 
+        public double YOffset
+        {
+            get { return (double)GetValue(YOffsetProperty); }
+            set { SetValue(YOffsetProperty, value); }
+        }
+
+        public object LayerContent
+        {
+            get { return (object)GetValue(LayerContentProperty); }
+            set { SetValue(LayerContentProperty, value); }
+        }
+
         public static readonly DependencyProperty XYContentProperty =
             DependencyProperty.Register("XYContent", typeof(object), typeof(ContentCube), new PropertyMetadata(null));
 
@@ -50,9 +65,45 @@ namespace Lils.WpfLib.Controls
         public static readonly DependencyProperty XZContentProperty =
             DependencyProperty.Register("XZContent", typeof(object), typeof(ContentCube), new PropertyMetadata(null));
 
+        public static readonly DependencyProperty LayerContentProperty =
+            DependencyProperty.Register("LayerContent", typeof(object), typeof(ContentCube), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty YOffsetProperty =
+            DependencyProperty.Register("YOffset", typeof(double), typeof(ContentCube), new PropertyMetadata(0.0, YOffsetPropertyChangedCallback, CoerceYOffsetPropertyCallback));
+
+        private static void YOffsetPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ContentCube cube)
+            {
+                if (cube.layerGeometryModel.Transform is TranslateTransform3D t3d)
+                {
+                    // TODO: replace this with binding
+                    t3d.OffsetY = (double)e.NewValue * 10;
+                }
+            }
+        }
+
+        private static object CoerceYOffsetPropertyCallback(DependencyObject d, object baseValue)
+        {
+            if ((double)baseValue < 0)
+            {
+                return 0.0;
+            }
+            else if ((double)baseValue > 1)
+            {
+                return 1.0;
+            }
+            else
+            {
+                return baseValue;
+            }
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+
+            layerGeometryModel = GetTemplateChild("PART_LayerModel3D") as GeometryModel3D;
         }
     }
 }
--- Lils.WpfLib/WpfLibTest/Samples/SliderSampleViewModel.cs
+++ Lils.WpfLib/WpfLibTest/Samples/SliderSampleViewModel.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using Prism.Mvvm;
 
 namespace WpfLibTest.Samples
 {
-    public class SliderSampleViewModel
+    public class SliderSampleViewModel : BindableBase
     {
-        
+        public double RedValuePercent { get; set; }
+
+        public byte RedValue => Convert.ToByte(RedValuePercent * 256);
+
+        public Color GreenMax => Color.FromRgb(RedValue, 255, 255);
+
+        public Color GreenMin => Color.FromRgb(RedValue, 0, 255);
     }
 }
