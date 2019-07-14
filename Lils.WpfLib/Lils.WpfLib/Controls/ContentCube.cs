110248fe4b43923a9ba5499b0f0049ff0d5035ab
lymims <lymims@outlook.com>
2019-07-14 15:34:47 +0800

    update ContentCube: Value upate

--- Lils.WpfLib/Lils.WpfLib/Controls/ContentCube.cs
+++ Lils.WpfLib/Lils.WpfLib/Controls/ContentCube.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
+using Lils.WpfLib.Tools;
 
 namespace Lils.WpfLib.Controls
 {
@@ -16,6 +18,8 @@ namespace Lils.WpfLib.Controls
     /// </summary>
     [TemplatePart(Name = "PART_CubeViewport", Type = typeof(Viewport3D))]
     [TemplatePart(Name = "PART_MoveYTranslate", Type = typeof(TranslateTransform3D))]
+    [TemplatePart(Name = "PART_ContentGrid", Type = typeof(Grid))]
+    [TemplatePart(Name = "PART_Pointer", Type = typeof(UIElement))]
     public class ContentCube : Control
     {
         static ContentCube()
@@ -25,6 +29,9 @@ namespace Lils.WpfLib.Controls
 
         private Viewport3D cubeViewport3D;
         TranslateTransform3D moveYTranslate;
+
+        private Grid contentGrid;
+        private UIElement pointer;
 
         public ContentCube()
         {
@@ -60,6 +67,30 @@ namespace Lils.WpfLib.Controls
             set { SetValue(LayerContentProperty, value); }
         }
 
+        public Point3D Maximum
+        {
+            get { return (Point3D)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public Point3D Minimum
+        {
+            get { return (Point3D)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public Point3D Value
+        {
+            get { return (Point3D)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public object Pointer
+        {
+            get { return (object)GetValue(PointerProperty); }
+            set { SetValue(PointerProperty, value); }
+        }
+
         public static readonly DependencyProperty XYContentProperty =
             DependencyProperty.Register("XYContent", typeof(object), typeof(ContentCube), new PropertyMetadata(null));
 
@@ -74,6 +105,18 @@ namespace Lils.WpfLib.Controls
 
         public static readonly DependencyProperty YOffsetProperty =
             DependencyProperty.Register("YOffset", typeof(double), typeof(ContentCube), new PropertyMetadata(0.0, YOffsetPropertyChangedCallback, CoerceYOffsetPropertyCallback));
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(Point3D), typeof(ContentCube), new PropertyMetadata(new Point3D(1, 1, 1)));
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(Point3D), typeof(ContentCube), new PropertyMetadata(new Point3D(0, 0, 0)));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(Point3D), typeof(ContentCube), new PropertyMetadata(new Point3D(0, 0, 0)));
+
+        public static readonly DependencyProperty PointerProperty =
+            DependencyProperty.Register("Pointer", typeof(object), typeof(ContentCube), new PropertyMetadata(null));
 
         private static void YOffsetPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -115,6 +158,12 @@ namespace Lils.WpfLib.Controls
                 Mouse.Capture(null);
             };
 
+            contentGrid = GetTemplateChild("PART_ContentGrid") as Grid;
+            contentGrid.MouseDown += ContentGrid_MouseAction;
+            contentGrid.MouseMove += ContentGrid_MouseAction;
+            contentGrid.MouseUp += ContentGrid_MouseUp;
+
+            pointer = GetTemplateChild("PART_Pointer") as UIElement;
         }
 
         private Point? previousPosition;
@@ -131,11 +180,39 @@ namespace Lils.WpfLib.Controls
                 var yDiff = currentPosition.Y - previousPosition.Value.Y;
                 yDiff *= 1.5;
                 var newCurrentValue = CoerceYOffsetPropertyCallback(null, YOffset - yDiff / cubeViewport3D.ActualHeight);
-                SetCurrentValue(YOffsetProperty, newCurrentValue);
+                YOffset = (double)newCurrentValue;
+                Value = new Point3D(Value.X, YOffset, Value.Z);
             }
 
             previousPosition = currentPosition;
             Mouse.Capture(cubeViewport3D);
+        }
+
+        private void ContentGrid_MouseAction(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+                return;
+
+            var mousePos = e.GetPosition(contentGrid);
+            var limit = new Point(contentGrid.ActualWidth, contentGrid.ActualHeight);
+            mousePos = mousePos.Coerce(new Point(0, 0), limit);
+
+            Canvas.SetLeft(pointer, mousePos.X);
+            Canvas.SetTop(pointer, mousePos.Y);
+
+            var ratioX = 1 - (mousePos.X / contentGrid.ActualWidth);
+            var ratioY = 1 - (mousePos.Y / contentGrid.ActualHeight);
+
+            var offset = Maximum - Minimum;
+            var scaledOffset = new Vector3D(offset.X * ratioX, YOffset, offset.Y * ratioY);
+
+            Value = scaledOffset + Minimum;
+            Mouse.Capture(contentGrid);
+        }
+
+        private void ContentGrid_MouseUp(object sender, MouseEventArgs e)
+        {
+            Mouse.Capture(null);
         }
     }
 }
